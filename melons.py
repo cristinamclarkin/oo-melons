bf5a06600d3cd10184b886b91ee01729032fc604
Hackbright Student <info@hackbrightacademy.com>
2016-04-14 18:02:27 -0700

    completed parts 1 and 2 of oo-melons

---
+++ melons.py
@@ -0,0 +1,70 @@
+"""This file should have our order classes in it."""
+
+
+class AbstractMelonOrder(object):
+    def __init__(self, species, qty, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+       
+
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+
+        if self.species == "Christmas Melon":
+            base_price = 1.5 * base_price
+     
+        
+    
+
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+        super(DomesticMelonOrder, self).__init__(species, qty, 'domestic', .08)
+        
+        
+        
+        
+
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+        super(InternationalMelonOrder, self).__init__(species, qty, 'international', .17)
+
+        self.country_code = country_code
+
+    def get_total(self):
+        total = super(InternationalMelonOrder, self).get_total()
+        if self.qty < 10:
+            return total + 3
+
+    
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+
+
+
+
